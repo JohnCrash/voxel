0e1cf40ec61b1a3f25dbc9721a13a986ff174049
zuzu2005 <udly@sina.com>
2017-08-26 00:56:54 +0800

    build script

--- build.js
+++ build.js
@@ -1,12 +1,25 @@
 var browserify = require('browserify');
 var fs = require('fs');
+const { spawn } = require('child_process');
 
-browserify('src/levelindex.js')
-.transform("babelify", {presets: ["es2015","es2016","es2017","react","stage-0"]})
-.bundle()
-.pipe(fs.createWriteStream('public/levelindex.js'));
+function build(src,des){
+  browserify(src)
+  .transform("babelify", {presets: ["es2015","es2016","es2017","react","stage-0"]})
+  .bundle()
+  .pipe(fs.createWriteStream(`${des}z`)
+  .on('close',()=>{
+    let c = spawn('java',['-jar','node_modules/google-closure-compiler/compiler.jar','--js',`${des}z`,'--js_output_file',des]);
+    c.stderr.on('data', (data) => {
+      console.log(`${data}`);
+    });
+    c.stdout.on('data', (data) => {
+      console.log(`${data}`);
+    }); 
+    c.on('close',(code)=>{
+      fs.unlink(`${des}z`);
+    });
+  }));  
+}
 
-browserify('src/editor.js')
-.transform("babelify", {presets: ["es2015","es2016","es2017","react","stage-0"]})
-.bundle()
-.pipe(fs.createWriteStream('public/editor.js'));  
+build('src/levelindex.js','public/levelindex.js');
+build('src/editor.js','public/editor.js');
